c23caf050eb710dec78826f41131e691cdd777cd
Hazem Ben Saria <bensariahazem@gmail.com>
2022-06-05 13:00:12 +0100

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const server = express()
+
+PORT  = 5000
+server.listen(PORT , _=>{console.log(`http://localhost:${PORT}`)
+
+})
+
+server.use("/" , (req ,res , next)=>{
+    
+    console.log("hazem")
+ 
+    next()
+})
+server.use("/" , (req, res)=>{
+   console.log("sddfg")
+   res.send('welcom hazem') 
+})
