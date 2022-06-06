7a7d1de9a922a3ef8b40ca88d0fd4667c2eb01d4
Hazem Ben Saria <bensariahazem@gmail.com>
2022-06-06 18:21:52 +0100

    still try

--- Modules/Routes/User.js
+++ Modules/Routes/User.js
@@ -319,11 +319,11 @@ route.get("",checkauth , (req ,res)=>{
 })
 
 route.post("/changephoto" , (req ,res )=>{
-    // const url = req.protocol+'://'+req.get('host') ;
-    // console.log(url)
-    // const id = req.userData.userId;
+    const url = req.protocol+'://'+req.get('host') ;
+    console.log(url)
+    const id = req.userData.userId;
     // const imagepath = url + '/images/'+req.file.filename;
-    res.json({img :'imagepath'})
+    res.json({img :'imagepath' , url : url , id: id})
     // console.log(imagepath)
     // Post.updateMany({userId:id} ,{$set:{userimage : imagepath}}).then(_=>{
     //     console.log('picts updates')})
