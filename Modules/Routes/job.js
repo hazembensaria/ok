a6164b987efab3ebdbfebc868eb4965ebfff1517
Hazem Ben Saria <bensariahazem@gmail.com>
2022-06-06 17:48:52 +0100

    add routes

--- Modules/Routes/job.js
+++ Modules/Routes/job.js
@@ -1,86 +1,103 @@
 const express = require('express')
 const route = express.Router()
-const Bug= require('../Models/bug')
-const Share= require('../Models/share')
+const Job= require('../Models/job')
+const Apply= require('../Models/apply')
+const User= require('../Models/User')
 const checkauth = require('../../middlewares/check-auth')
-const User = require("../Models/User");
+const Post = require("../Models/post");
 
 
 
 route.post('/add',checkauth,(req,res)=>{
-    const  comment = new Bug({
-
+    const  comment = new Job({
+        level:req.body.level,
         name :req.body.name,
-        userimg :req.body.imagepath,
+        imagepath :req.body.imagepath,
         iduser : req.userData.userId,
+        location:req.body.location,
+        type:req.body.type,
         content :req.body.content,
-        title :req.body.title,
-        tags : req.body.tags
-    }).save().then(bug=>{
+        salary :req.body.salary,
+        tech : req.body.tech
+    }).save().then(job=>{
         res.status(200).json({cm :comment})
     },err=>{
         console.log(err)
     })
 })
-
-
 route.get("" ,(req ,res)=>{
-Bug.find().sort({date : -1}).then(bugs=>{
-        res.status(201).json(bugs)
+    Job.find().sort({date : -1}).then(jobs=>{
+        res.status(201).json(jobs)
     },err=>{
         console.log('something went wrong '+ err)
     })
 })
 
-route.post("/get" ,(req ,res)=>{
-    Bug.findById(req.body.id).then(bugs=>{
-        res.status(201).json(bugs)
+route.get("/userJobs" ,checkauth,(req ,res)=>{
+    Job.find({iduser :req.userData.userId}).sort({date : -1}).then(jobs=>{
+        res.status(201).json(jobs)
     },err=>{
         console.log('something went wrong '+ err)
     })
 })
 
-route.post('/share',checkauth,(req,res)=>{
 
-    for(let i of req.body.ids){
-        const share = new Share({
-            imagepath :req.body.image,
-            title :req.body.title,
-            name:req.body.name,
-            idpost : req.body.idBug,
-            idshare : req.userData.userId,
-            idreceve : i ,
-        }).save().then(share=>{
-            console.log(share)
+route.post('/apply',checkauth,(req,res)=> {
+        Job.updateOne({_id:req.body.idpost},{$inc :{applied :1}}).then(res=>{
+            console.log('added 1 to job')
+        })
+    const apply = new Apply({
+        idpost:req.body.idpost,
+        iduser:req.userData.userId,
+        idposter:req.body.iduser
+    }).save().then(apply=>{
+        console.log('apply added succefuly')
+    }, err=>{
+        console.log(err)
+    })
+})
+route.post('/sugest',checkauth,(req,res)=> {
+    Job.find({tech:{$in: req.body.lang}}).then(jobs=>{
+        res.status(200).json(jobs)
+    },err=>{
+        console.log(err);
+    })
+
+    })
+route.post('/showApplied',checkauth,(req,res)=> {
+    Apply.find({idpost: req.body.idJob}).then(apply=>{
+        console.log(apply)
+        const ids= []
+        for(let appli of apply){
+            ids.push(appli.iduser)
+        }
+        console.log(ids)
+        User.find({_id:{$in: ids}}).then(users=>{
+            res.status(200).json(users)
         },err=>{
-
             console.log(err)
         })
+    },err=>{
+        console.log(err);
+    })
+
+})
+
+route.post('/switchStatus',checkauth,(req,res)=> {
+    if(req.body.status){
+        Job.updateOne({_id: req.body.idJob},{$set:{full:false}}).then(result=>{
+            console.log("comments status aupdated successfuly")})
+    }else{
+        Job.updateOne({_id: req.body.idJob},{$set:{full:true}}).then(result=>{
+            console.log("comments status aupdated successfuly")})
     }
 
+
+})
+route.post('/deleteJob',checkauth,(req,res)=> {
+    Job.deleteOne({_id: req.body.idJob}).then(result=>{
+        console.log("deleted successfuly")})
+
 })
 
-route.get("/getShares" ,checkauth,(req ,res)=>{
-    Share.find({idreceve :req.userData.userId}).sort({date : -1}).then( shares => {
-        console.log('shares' + shares)
-        const ids = []
-        for (let i of shares) {
-            console.log('one share ' + i.idpost)
-             Bug.find({_id: i.idpost}).then(bugs => {
-
-                User.find({_id: i.idshare}).then(user => {
-                    obj = {
-                        post: bugs,
-                        user: user
-                    }
-                    ids.push(obj)
-                })
-
-            })
-            console.log(ids)
-
-        }
-        res.status(200).json(shares)
-    })
-})
 module.exports = route
